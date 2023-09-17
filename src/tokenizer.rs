1aef3b6165e2fc35fed6080ce77d8a41c86024b0
kirill <polferov@live.com>
2023-09-17 18:15:07 +0200

    tokenize strings identifiers and parentheses

--- src/tokenizer.rs
+++ src/tokenizer.rs
@@ -2,6 +2,12 @@
 pub enum TokenValue {
     Semicolon,
     Integer(i32),
+    Newline,
+    StringLiteral(String),
+    Space,
+    Identifier(String),
+    RoundOpen,
+    RoundClose,
 }
 
 #[derive(Debug, PartialEq)]
@@ -33,27 +39,62 @@ impl Tokenizer {
         self.input.chars().nth(self.position)
     }
 
+    fn read_string_literal(&mut self) -> Token {
+        let mut value = String::new();
+        while let Some(c) = self.take() {
+            if c == '"' {
+                break;
+            }
+            value.push(c);
+        }
+        Token { value: TokenValue::StringLiteral(value) }
+    }
+
+    fn single_char_token_value(c: char) -> Option<TokenValue> {
+        match c {
+            ';' => Some(TokenValue::Semicolon),
+            '\n' => Some(TokenValue::Newline),
+            ' ' => Some(TokenValue::Space),
+            '(' => Some(TokenValue::RoundOpen),
+            ')' => Some(TokenValue::RoundClose),
+            _ => None,
+        }
+    }
 
     fn next_token(&mut self) -> Option<Token> {
         let c = self.take()?;
-        // handle single character tokens
-        match c {
-            ';' => return Some(Token { value: TokenValue::Semicolon }),
-            _ => (),
-        };
 
+        // handle single character tokens
+        if let Some(token_value) = Self::single_char_token_value(c) {
+            return Some(Token { value: token_value });
+        }
+
+        // handle string literals
+        if c == '"' {
+            return Some(self.read_string_literal());
+        }
+
+        // accumulate characters into a string
         let mut value = String::new();
         value.push(c);
         while let Some(c) = self.peek() {
-            match c {
-                ';' => break,
-                _ => value.push(self.take().unwrap()),
+            if Self::single_char_token_value(c).is_some() {
+                break;
             }
+            value.push(self.take()?);
         }
+
+
         // handle integer tokens
         if let Ok(i) = value.parse::<i32>() {
             return Some(Token { value: TokenValue::Integer(i) });
         }
+
+        // handle identifiers
+        if value.chars().all(|c| c.is_alphanumeric()) {
+            return Some(Token { value: TokenValue::Identifier(value) });
+        }
+
         panic!("Unexpected token: {}", value);
     }
 
